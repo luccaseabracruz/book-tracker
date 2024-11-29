2dd580a9bf254cbf9b12c2e8f15dc035e93e9793
Lucca Cruz <114677146+luccaseabra@users.noreply.github.com>
2024-11-29 15:41:29 +0000

    Update GetAllBooksUseCase to return an ordered books list.

--- app/src/main/java/com/example/booktracker/domain/usecase/GetAllBooksUseCase.kt
+++ app/src/main/java/com/example/booktracker/domain/usecase/GetAllBooksUseCase.kt
@@ -3,8 +3,10 @@ package com.example.booktracker.domain.usecase
 import com.example.booktracker.domain.model.BookDomain
 import com.example.booktracker.domain.repository.BookRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
-class GetAllBooksUseCase @Inject constructor(private val repository: BookRepository){
-    suspend operator fun invoke(): Flow<List<BookDomain>> = repository.getAllBooks()
+class GetAllBooksUseCase @Inject constructor(private val repository: BookRepository) {
+    suspend operator fun invoke(): Flow<List<BookDomain>> =
+        repository.getAllBooks().map { books -> books.sortedBy { it.title.lowercase() } }
 }
